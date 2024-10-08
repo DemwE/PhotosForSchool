8596af8e8d2c0e785d5e8131daf544bd352c34d0
DemwE <mateuszczarnecki360@gmail.com>
2024-10-08 18:38:07 +0200

    progress

--- src/components/gallery.tsx
+++ src/components/gallery.tsx
@@ -4,19 +4,23 @@ import React from "react";
 import { useEffect, useState } from 'react';
 import ModalImage from "@/components/modalImage"
 
+function shuffleArray(array: string[]): string[] {
+  return array.sort(() => Math.random() - 0.5);
+}
+
 export default function Gallery({ images }: { images: Promise<string[]> }) {
   const [imageList, setImageList] = useState<string[]>([]);
 
   useEffect(() => {
-    images.then((images) => setImageList(images));
+    images.then((images) => setImageList(shuffleArray(images)));
   }, [images]);
 
   return (
     <section id="Gallery" className="px-6 md:px-14 pt-24 items-center space-y-16">
       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {imageList.map((image, index) => (
-            <ModalImage key={index} src={image} alt="Photo" className="h-96 xl:h-[26rem]"/>
-          ))}
+        {imageList.map((image, index) => (
+          <ModalImage key={index} src={image} alt="Photo" className="h-96 xl:h-[26rem]"/>
+        ))}
       </div>
     </section>
   );
